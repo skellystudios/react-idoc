9afe9d25ff3e1e2aa211ca540ca7255350134289
Michael Skelly <skellystudios@gmail.com>
2015-11-29 15:00:41 +0000

    Labels working and looking nice

--- js/components/IDocMap.react.js
+++ js/components/IDocMap.react.js
@@ -10,13 +10,9 @@ var ReactPropTypes = React.PropTypes;
 var Actions = require('../actions/Actions');
 var LocationStore = require('../stores/LocationStore');
 var Categories = require('../stores/Categories');
-
-
-
 var cx = require('react/lib/cx');
 
 var mapStyles = [{"featureType":"all","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}]
-
 
 var IDocMap = React.createClass({
 
@@ -24,9 +20,6 @@ var IDocMap = React.createClass({
 
   getInitialState: function(){
     return {
-      zoom: 12,
-      center: new google.maps.LatLng(51.511523, -0.156728),
-      mapStyles: this.props.mapStyles,
       allPoints: this.props.allPoints,
     };
   },
@@ -39,14 +32,13 @@ var IDocMap = React.createClass({
     LocationStore.removeChangeListener(this._onChange);
   },
 
-  // shouldComponentUpdate: function(nextProps, nextState) {
-  //    return true;
-  // },
-
   render: function() {
     var markers = [];
-    var allPoints = this.state.allPoints
-    for (var key in allPoints) {
+    for (var key in this.state.allPoints) {
+      
+      var point = this.state.allPoints[key];
+      var category = Categories[point.category];
+
       var icon = {
         fillColor: '#FF0000',
         fillOpacity: .6,
@@ -54,14 +46,15 @@ var IDocMap = React.createClass({
         strokeWeight: 1,
         scale: 1,
       }
-      var point = allPoints[key];
-      var category = Categories[point.category];
       icon.url = category.icon_url;
       icon.scaledSize = new google.maps.Size(category.width,category.height); 
+      
+
       if (point.visible) {
+        
         var position = new google.maps.LatLng(point.lat, point.long);
         var visible = point.visible; 
-        var key1 = key + "1"
+        var label_key = key + "_label"
         //var visible = true;
         markers.push(
           <Marker key={key} ref={key} position={position} 
@@ -71,32 +64,28 @@ var IDocMap = React.createClass({
         markers.push(
           <OverlayView 
           position={position} 
-          key={key1}
+          key={label_key}
           mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
           >
             <div 
             className="marker-label"
             >
-              {point.title}
+              <p>
+                {point.title}
+              </p>
             </div>
           </OverlayView>
         );
         
       }
     }
-    // {...this.props}
+
     return <div  className="map" style={{height: "100%"}} >
         <GoogleMap ref="map" style={{height: "100%"}}
-              containerProps={{
-                style: {
-                  height: "100%",
-                }
-              }}
-              zoom={this.state.zoom} 
+              containerProps={{ style: { height: "100%", } }}
+              defaultZoom={12} 
               defaultCenter={{lat: 51.511523, lng: -0.156728}} 
-              defaultOptions={{
-                styles: mapStyles,
-              }}
+              defaultOptions={{ styles: mapStyles, }}
               >
         {markers}
         </GoogleMap>
@@ -115,9 +104,6 @@ var IDocMap = React.createClass({
   }
 
 });
-
-IDocMap.defaultProps = {
-}
 
 module.exports = IDocMap;
 
